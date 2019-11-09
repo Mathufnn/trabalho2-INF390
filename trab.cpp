c27a608972509ae68eb865a78d1c6fd2f86cb43c
Mayke Moreira <mayke.moreira3@gmail.com>
2019-11-09 18:47:09 -0300

    testes com lookat

--- trab.cpp
+++ trab.cpp
@@ -2,7 +2,21 @@
 #include <GL/glut.h> // compilar com -lGL -lGLU -lglut
 using namespace std;
 
-// Coisas do opengl
+
+GLdouble eyex = 0.0;
+GLdouble eyey = 0.0;
+GLdouble eyez = 0.0;
+GLdouble centerX = 0.0;
+GLdouble centerY = 0.0;
+GLdouble centerZ = 0.0;
+GLdouble upX = 0.0;
+GLdouble upY = 0.0;
+GLdouble upZ = 0.0;
+
+
+
+
+
 
 struct vertice
 {
@@ -143,17 +157,45 @@ void keyboard(unsigned char key, int x, int y)
   {
     rotacionar = !rotacionar;
   }
+
+
+
 }
+
+
+
+void keyboard2( int key, int x ,int y){
+
+ if (key == GLUT_KEY_UP) {
+   eyez++;
+   eyex++;
+   eyey++;
+ }
+
+ if (key == GLUT_KEY_DOWN){
+   eyez--;
+    eyey--;
+   eyex--;
+ }
+
+ cout << eyex << " " << eyey << " " << eyez << endl;
+
+
+}
+
 
 void display()
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
+
+  gluLookAt(0,0,-4,0.0,0,1,0,1,0);
+  gluPerspective(0,1.33,0,100);
   glTranslatef(0.0f, 0.0f, -2.0f);
 
-  glRotatef(-90, 1, 0, 0);
-  glRotatef(rotacao, 0, 0, 1);
+//  glRotatef(-90, 1, 0, 0);
+ // glRotatef(rotacao, 0, 0, 1);
 
   glColor3f(1, 1, 0);
   glBegin(GL_TRIANGLES);
@@ -196,6 +238,7 @@ int main(int argc, char **argv)
   glutDisplayFunc(display);
   glutReshapeFunc(reshape);
   glutKeyboardFunc(keyboard);
+  glutSpecialFunc(keyboard2);
   initGL();
   glutTimerFunc(100, rodarSubmarino, 0);
   glutMainLoop();
